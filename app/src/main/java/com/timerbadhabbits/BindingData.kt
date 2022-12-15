aec9212f4ae662f81bf119dc4ea63e1f637d4de9
Danil_M <max.cacz@yandex.ru>
2022-12-16 00:43:55 +0500

    [logic] add sharedPref

--- app/src/main/java/com/timerbadhabbits/BindingData.kt
+++ app/src/main/java/com/timerbadhabbits/BindingData.kt
@@ -3,4 +3,14 @@ package com.timerbadhabbits
 import android.content.Context
 import com.timerbadhabbits.databinding.ActivityMainBinding
 
-class BindingData(private val context: Context, private val binding: ActivityMainBinding)
+
+class BindingData(private val context: Context, private val binding: ActivityMainBinding) {
+
+    fun action() {
+
+        val sharedPref = context.getSharedPreferences("main",Context.MODE_PRIVATE)
+
+    }
+
+
+}
