c60bc4be0f9048fe04fc0dfc3484887c459f4d65
Danil_M <max.cacz@yandex.ru>
2022-12-16 00:08:44 +0500

    [add] adding initial methods

--- app/src/main/java/com/timerbadhabbits/BindingData.kt
+++ app/src/main/java/com/timerbadhabbits/BindingData.kt
@@ -3,10 +3,4 @@ package com.timerbadhabbits
 import android.content.Context
 import com.timerbadhabbits.databinding.ActivityMainBinding
 
-class BindingData(private val context: Context, private val binding: ActivityMainBinding) {
-
-
-
-
-
-}
+class BindingData(private val context: Context, private val binding: ActivityMainBinding)
--- app/src/main/java/com/timerbadhabbits/MainActivity.kt
+++ app/src/main/java/com/timerbadhabbits/MainActivity.kt
@@ -2,10 +2,40 @@ package com.timerbadhabbits
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.timerbadhabbits.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {  }
+
+    private val bindingData by lazy {  }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    private fun initBinding() {
+
+
+
+
+
+    }
+
+
+    private fun initBindingData() {
+
+
+
+
+
+    }
+
+
+
+
 }
