01eb4fdd72bb43e79851b8696560f9db69d1f680
Danil_M <max.cacz@yandex.ru>
2022-12-16 00:04:14 +0500

    [add] adding binding class

---
+++ app/src/main/java/com/timerbadhabbits/BindingData.kt
@@ -0,0 +1,12 @@
+package com.timerbadhabbits
+
+import android.content.Context
+import com.timerbadhabbits.databinding.ActivityMainBinding
+
+class BindingData(private val context: Context, private val binding: ActivityMainBinding) {
+
+
+
+
+
+}
